24f799dcd9a5da83fafe38199be285d7a091e62f
nish316 <smash@Nishkas-MacBook-Air.local>
2025-09-21 14:23:53 -0700

    dlasfkj

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nishkaOp.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "RegularOpMode")
+public class nishkaOp extends OpMode {
+    private DcMotor frontLeft, backLeft, frontRight, backRight;
+    public void init()
+    {
+        frontLeft  = hardwareMap.get(DcMotor.class, "frontLeft");
+        backLeft   = hardwareMap.get(DcMotor.class, "backLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backRight  = hardwareMap.get(DcMotor.class, "backRight");
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        backLeft.setDirection(DcMotor.Direction.FORWARD);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addLine("Init complete");
+    }
+    @Override
+    public void loop()
+    {
+        double forward = gamepad1.left_stick_y;
+        double turn = gamepad1.right_stick_x;
+
+    }
+
+}
