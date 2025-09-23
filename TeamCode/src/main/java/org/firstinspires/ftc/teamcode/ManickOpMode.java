4d6b06b3c02d284d76d2e747e83ac3a39025a7ea
IW-Manick <manickavasaganb@gmail.com>
2025-09-22 17:38:20 -0700

    manick

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManickOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManickOpMode.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name = "ManickOpMode")
-public class MecanumDriveExample extends LinearOpMode {
+public class ManickOpMode extends LinearOpMode {
 
    
     private DcMotor frontLeft = null;
