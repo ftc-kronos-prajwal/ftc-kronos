c9ada16f95cdcbbbfc8cf038ffd5d22b20f94ac5
idk <developincpp@gmail.com>
2025-09-22 17:22:29 -0700

    idk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrajwalOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrajwalOpMode.java
@@ -37,51 +37,6 @@ public class PrajwalOpMode extends OpMode {
     }
     @Override
     public void loop(){
-        /*float rightAmount = gamepad1.left_stick_y, leftAmount = gamepad1.left_stick_y;
-        float xFactor = gamepad1.left_stick_x*2-1;
-
-        rightAmount *= xFactor;
-        leftAmount *= xFactor;
-        if(Math.abs(gamepad1.left_stick_y) <= 0.1 && Math.abs(gamepad1.left_stick_x) >= 0.1){
-            rightAmount = gamepad1.left_stick_x;
-            leftAmount = -gamepad1.left_stick_x;
-        }
-
-        motors[0].setPower(leftAmount);
-        motors[1].setPower(leftAmount);
-        motors[2].setPower(rightAmount);
-        motors[3].setPower(rightAmount);*/
-
-
-        /*
-        float rightAmount = gamepad1.left_stick_y - gamepad1.right_stick_x, leftAmount = gamepad1.left_stick_y + gamepad1.right_stick_x;
-
-        if(gamepad1.left_stick_x >= 0.1){
-            motors[0].setPower(leftAmount);
-            motors[1].setPower(-leftAmount);
-            motors[2].setPower(-rightAmount);
-            motors[3].setPower(rightAmount);
-        }else if(gamepad1.left_stick_x <= -0.1){
-            motors[0].setPower(-leftAmount);
-            motors[1].setPower(leftAmount);
-            motors[2].setPower(rightAmount);
-            motors[3].setPower(-rightAmount);
-        }else{
-            motors[0].setPower(leftAmount);
-            motors[1].setPower(leftAmount);
-            motors[2].setPower(rightAmount);
-            motors[3].setPower(rightAmount);
-
-        }
-         */
-
-
-        /*
-        top left...
-         diagonal 1: 0 1 1 1 0 -1 -1 -1
-         diagonal 2: 1 1 0 -1 -1 -1 0 1
-         */
-
         float diag1 = gamepad1.left_stick_y + gamepad1.left_stick_x, diag2 = gamepad1.left_stick_y - gamepad1.left_stick_x;
 
         motors[0].setPower(diag1+gamepad1.right_stick_x);
