0f033161af0a63c0c2a31203054d3f8143b5f95a
idk <developincpp@gmail.com>
2025-10-06 17:30:13 -0700

    prajwal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SurajOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SurajOpMode.java
@@ -1,10 +1,9 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="SurajOpMode")
 public class SurajOpMode extends OpMode {
