83099b9576abddf655e69a3b27e6e31cff407f04
idk <developincpp@gmail.com>
2025-10-10 17:22:53 -0700

    prajwal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TwoWheelTrackingLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TwoWheelTrackingLocalizer.java
@@ -34,15 +34,15 @@ import java.util.List;
  *
  */
 public class TwoWheelTrackingLocalizer extends TwoTrackingWheelLocalizer {
-    public static double TICKS_PER_REV = 383.6;
+    public static double TICKS_PER_REV = 384.5;
     public static double WHEEL_RADIUS = 1.88976; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double PARALLEL_X = 4; // X is the up and down direction
-    public static double PARALLEL_Y = 2; // Y is the strafe direction
+    public static double PARALLEL_X = 1.5; // X is the up and down direction
+    public static double PARALLEL_Y = -1; // Y is the strafe direction
 
-    public static double PERPENDICULAR_X = 0;
-    public static double PERPENDICULAR_Y = 0;
+    public static double PERPENDICULAR_X = -1;
+    public static double PERPENDICULAR_Y = -1;
 
     // Parallel/Perpendicular to the forward axis
     // Parallel wheel is parallel to the forward axis
