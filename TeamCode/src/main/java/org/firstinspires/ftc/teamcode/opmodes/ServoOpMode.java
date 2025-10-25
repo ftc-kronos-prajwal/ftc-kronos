ebc839e32e14675dd6f1e8b226f6ea83d3d217e5
idk <developincpp@gmail.com>
2025-10-24 17:30:19 -0700

    prajwal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ServoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ServoOpMode.java
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
-
 @TeleOp(name="ServoOpMode")
 public class ServoOpMode extends OpMode {
     Servo servo = null;
