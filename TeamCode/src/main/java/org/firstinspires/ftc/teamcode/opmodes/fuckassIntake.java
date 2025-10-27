e00f36582cfae2770642958ba7acdd6e0855da50
saina-singh24 <singh.saina@gmail.com>
2025-10-27 16:57:10 -0700

    fuckassIntake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/fuckassIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/fuckassIntake.java
@@ -1,4 +1,25 @@
-package org.firstinspires.ftc.teamcode.opmodes;
+`
+private DcMotor intakeMotor;
 
-public class fuckassIntake  {
+@Override
+public void init() {
+
+    intakeMotor = hardwareMap.get(DcMotor.class, "intake");
+    intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    intakeMotor.setDirection(DcMotor.Direction.FORWARD); 
+}
+
+@Override
+public void loop() {
+    
+    if (gamepad1.a) {
+        intakeMotor.setPower(1.0);  
+    } else if (gamepad1.b) {
+        intakeMotor.setPower(-1.0); 
+    } else {
+        intakeMotor.setPower(0);   
+    }
+
+    telemetry.addData("Intake Power", intakeMotor.getPower());
+    telemetry.update();
 }
