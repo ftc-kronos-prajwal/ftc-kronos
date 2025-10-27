fd875857bafed69d77c5f82fff9898ce868cb279
ftckronosprogramming <ftckronosprogramming@gmail.com>
2025-10-26 17:38:03 -0700

    prajwal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/CameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/CameraTest.java
@@ -64,7 +64,6 @@ import java.util.List;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
 @TeleOp(name = "CameraTest")
-@Disabled
 public class CameraTest extends LinearOpMode {
 
     private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
