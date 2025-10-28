e783f2f1adc3c79e58252ddc40578efd3e2bc4af
ftckronosprogramming <ftckronosprogramming@gmail.com>
2025-10-27 18:19:29 -0700

    prajwal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/fuckassIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/fuckassIntake.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="fuckassIntake")
 public class fuckassIntake extends OpMode {
