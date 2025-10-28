9dc9b184a4a0b300a376468f1c48bdd4345b98dc
ftckronosprogramming <ftckronosprogramming@gmail.com>
2025-10-27 17:42:46 -0700

    prajwal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/CameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/CameraTest.java
@@ -159,7 +159,7 @@ public class CameraTest extends LinearOpMode {
         }
 
         // Choose a camera resolution. Not all cameras support all resolutions.
-        //builder.setCameraResolution(new Size(640, 480));
+        builder.setCameraResolution(new Size(1920, 1080));
 
         // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
         //builder.enableLiveView(true);
