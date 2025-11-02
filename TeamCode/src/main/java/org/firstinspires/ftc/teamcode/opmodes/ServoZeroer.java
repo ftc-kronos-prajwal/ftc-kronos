87219a170f8382ec6a452c47e2c0b15a9675d5d5
idk <developincpp@gmail.com>
2025-11-02 15:41:41 -0800

    prajwal

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ServoZeroer.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name="ServoZeroer")
+public class ServoZeroer extends OpMode {
+    Servo servo = null;
+
+    @Override
+    public void init(){
+        servo = hardwareMap.get(Servo.class, "servo");
+        servo.setPosition(0);
+    }
+
+    @Override
+    public void loop(){
+    }
+}
