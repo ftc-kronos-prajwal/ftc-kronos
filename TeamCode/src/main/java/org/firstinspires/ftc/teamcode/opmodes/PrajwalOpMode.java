46fce860983bb7681e3e4b50c549a445d9270ac7
idk <developincpp@gmail.com>
2025-11-04 18:59:31 -0800

    prajwal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/PrajwalOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/PrajwalOpMode.java
@@ -144,6 +144,8 @@ public class PrajwalOpMode extends OpMode {
                 intakeServo.setPosition(0);
                 intakeServoPosition = 0;
             }
+        }else{
+            intakeMotor.setPower(0);
         }
 
         telemetry.addLine("-------INTAKE------");
