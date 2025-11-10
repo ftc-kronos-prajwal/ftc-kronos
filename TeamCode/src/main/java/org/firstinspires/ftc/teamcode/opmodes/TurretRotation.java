294c201c6bf50d8c068c70b907f73f2b856375a6
ftckronosprogramming <ftckronosprogramming@gmail.com>
2025-11-09 17:55:51 -0800

    prajwal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TurretRotation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TurretRotation.java
@@ -22,7 +22,7 @@ public class TurretRotation extends OpMode {
         if (gamepad1.right_trigger > 0) {
             power = gamepad1.right_trigger;
         } else if (gamepad1.left_trigger > 0){
-            power = gamepad1.left_trigger;
+            power = -gamepad1.left_trigger;
         }
         leftServo.setPower(power);
         rightServo.setPower(power);
